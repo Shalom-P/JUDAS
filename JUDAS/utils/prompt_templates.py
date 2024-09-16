32e30c729a5b33d4bd2e77bb1b6797b3f8b6c7cb
Shalom-P <rahulnalluru007@gmail.com>
2024-09-16 22:31:45 +0530

    HAVE MADE CHANGES TO THE PROMPT TO FIX SOME CONVERSATION ISSUES

--- JUDAS/utils/prompt_templates.py
+++ JUDAS/utils/prompt_templates.py
@@ -67,8 +67,6 @@ def get_prompt_for_spotify(conv_history:list,query: str) -> str:
 def get_prompt_conversation(conv_history:list,query:str) -> str:
     system_template=f'''<|begin_of_text|><|start_header_id|>system<|end_header_id|>
         Your name is judas, you are a kind and polite conversational assistant.
-        Use this conversation history as context to assist the user if any.
-        conversation history:{conv_history}
         Your task is to follow the following instructions:
         1) Analyze the user's input and use the conversation history as context to better understand the user's query and decide if the situation warrants a response.
         2) The response should be in the JSON format. where the key is the kind of response and the value is the response itself, the key can be only one of these three tags [OPINION,NO_OPINION,ANSWER].
@@ -77,20 +75,20 @@ def get_prompt_conversation(conv_history:list,query:str) -> str:
         5) The rest of the times you will decide if the situation warrants an opinion or not.
         6) If the situation warrants an opinion, you will use OPINION tag.
         7) If the situation does not warrant an opinion, you will use NO_OPINION tag and the value will be "no opinion".
-        <|eot_id|>'''
+        Use this conversation history as context to assist the user if any.
+        conversation history:{conv_history}<|eot_id|>'''
     prompt_template = f'''{system_template}
         <|start_header_id|>user<|end_header_id|>{query}<|eot_id|><|start_header_id|>judas<|end_header_id|>'''
     return prompt_template
 
 def get_prompt_decide(query:str,conv_history:list)->str:
     prompt_template=f'''<|begin_of_text|><|start_header_id|>system<|end_header_id|>your name is judas.
-        Use the conversation history as context.
-        conversation history:{conv_history}
         You are a smart assistant that classifies user requests into two categories: '@NOT_CONTROL_SONGS@' and '@CONTROL_SONGS@'.
         Given any user input, you need to determine whether the user is asking for general assistance or if they are requesting music-related actions such as playback or controlling music.
         if the request involves playing music, controlling playback (e.g., play, pause, stop, next, previous), or actions related to liking or unliking a song, classify it as '@CONTROL_SONGS@'.
         For all other requests, including discussing music, lyrics, songs, artists, albums or general assistance, classify it as '@NOT_CONTROL_SONGS@'.
         Output only one of the two tags: '@NOT_CONTROL_SONGS@' or '@CONTROL_SONGS@'. Do not provide any explanations, only the tag.
-        <|eot_id|>
+        Use the conversation history as context.
+        conversation history:{conv_history}<|eot_id|>
         <|start_header_id|>user<|end_header_id|>{query}<|eot_id|><|start_header_id|>judas<|end_header_id|>:'''
     return prompt_template
